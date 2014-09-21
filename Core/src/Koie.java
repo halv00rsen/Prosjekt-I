8ff996bdb992a3cda6432f8d5f9d1540ac462301
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-09-21 15:53:57 +0200

    enkel funksjonalitet i koieobjektet

--- Core/src/Koie.java
+++ Core/src/Koie.java
@@ -1,9 +1,27 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Koie {
 	public final String name;
+	public final int numBeds;
 	
-	public Koie(String name){
+	private final List<Equipment> equipment;
+	private final Calendar cabinRented;
+	
+	public Koie(String name, int numBeds){
 		this.name = name;
+		this.numBeds = numBeds;
+		equipment = new ArrayList<Equipment>();
+		cabinRented = new Calendar();
+	}
+	
+	public boolean cabinIsRented(int day, int month){
+		return cabinRented.dayIsTaken(day, month);
+	}
+	
+	public void reserveCabin(int dayFrom, int monthFrom, int numOfDays){
+		
 	}
 }
