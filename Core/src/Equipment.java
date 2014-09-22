6befeb514255308041d594bdc264b22e22b0cdf9
thomahan <thomasrenehansen@gmail.com>
2014-09-22 12:59:07 +0200

    Integrerte Equipment som fullstendig klasse.

--- Core/src/Equipment.java
+++ Core/src/Equipment.java
@@ -1,12 +1,18 @@
 package src;
 
 public abstract class Equipment {
-	
+        private static String name;
 	private boolean broken;
-	
+
+	public Equipment(String name){
+	        this.name = name;	
+	}
+
 	public boolean isBroken(){
 		return broken;
 	}
-	
-	public abstract String getName();
+
+	public String getName(){
+		return name;
+	}
 }
