e5e417435be8c6003a3dcba70f07dd0586a8520e
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-09-24 14:26:16 +0200

    Ferdig testet med getValidDate()

--- Core/src/Calendar.java
+++ Core/src/Calendar.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.GregorianCalendar;
+
 public class Calendar {
 	
 	public final static int daysInFeature = 180;
@@ -15,11 +17,17 @@ public class Calendar {
 	public static boolean validDate(int day, int month){
 		if (day < 1 || month < 1 || month > 12)
 			return false;
-		if (month == 2 && day <= 28){
-			return true;
-		}else if ((month < 8 && month % 2 == 1) || (month > 7 && month % 2 == 1)){
-			return true;
+		if (month == 2){
+			java.util.Calendar c = new GregorianCalendar();
+			if (c.getWeekYear() % 4 == 0)
+				return day <= 29;
+			return day <= 28;
+		}else if ((month < 8 && month % 2 == 1) || (month > 7 && month % 2 == 0)){
+			return day <= 31;
 		}
-		return true;
+		else if ((month < 8 && month % 2 == 0) || (month > 7 && month % 2 == 1)){
+			return day <= 30;
+		}
+		return false;
 	}
 }
