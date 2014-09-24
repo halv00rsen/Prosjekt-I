4159d5239d16d78d5493850973c5f6c2962ce128
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-09-24 14:57:08 +0200

    Lagde et datoObjekt

--- Core/src/TestMain.java
+++ Core/src/TestMain.java
@@ -3,6 +3,12 @@ package src;
 public class TestMain {
 	
 	public static void main(String[] args){
-		
+		String[] hei = new String[] {"Januar", "Februar", "Mars", "April","Mai", "Juni", "July", "August", "September", "Oktober", "November", "Desember"};
+		for (int a = 0; a < 12; a++){
+			System.out.println(hei[a]);
+			for (int b = -2; b < 35; b++){
+				System.out.println("Day: " + b + ", isValid: " + Calendar.validDate(b, a + 1));
+			}
+		}
 	}
 }
