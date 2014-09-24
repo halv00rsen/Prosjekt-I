6a880a3cef9a87bfbbef826890024f2626c2d2c8
thomahan <thomasrenehansen@gmail.com>
2014-09-24 15:02:44 +0200

    Utkommenterte linjer

--- Core/tests/EquipmentTest.java
+++ Core/tests/EquipmentTest.java
@@ -1,10 +1,12 @@
 package tests;
 
+//import src.Equipment;
+
 public class EquipmentTest {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Test.");
+		//int x = 2;
+		System.out.println("a");
 		
 	}
 
