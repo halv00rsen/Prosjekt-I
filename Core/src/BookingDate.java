c0d4e74d771992e1ed91176196ab84bc731b279d
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-09-30 22:40:35 +0200

    datesCollide er fikset

--- Core/src/BookingDate.java
+++ Core/src/BookingDate.java
@@ -5,14 +5,19 @@ public class BookingDate {
 	public final Date dateFrom, dateTo;
 	public final int numberOfDays;
 	
-	public BookingDate(Date dateFrom, Date dateTo){
+	public BookingDate(Date dateFrom, Date dateTo) throws IllegalArgumentException{
 		this.dateFrom = dateFrom;
 		this.dateTo = dateTo;
 		numberOfDays = Calendar.getNumOfDaysBetween(dateFrom, dateTo);
+		if (numberOfDays > Calendar.daysInFeature)
+			throw new IllegalArgumentException("not valid dates for the program");
 	}
 	
-	public boolean datesCollideWithBooking(Date dateFrom, Date dateTo){
-		int numDays = Calendar.getNumOfDaysBetween(dateFrom, dateTo);
-		return true;
+	public boolean datesCollideWithBooking(Date dateFrom, Date dateTo){//sjekker om datoer sendt inn kolliderer med denne bookingen
+		return dateFrom.isAfter(this.dateTo) || dateTo.isBefore(this.dateFrom);
+	}
+	
+	public String toString(){
+		return "[DateFrom: " + dateFrom + ", DateTo: " + dateTo + ", Days: " + numberOfDays + "]";
 	}
 }
