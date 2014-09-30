7c2a52e801c53124fa41b8f89562dd32374768c5
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-01 01:53:23 +0200

    La til kommentarer

--- Core/src/BookingDate.java
+++ Core/src/BookingDate.java
@@ -13,14 +13,17 @@ public class BookingDate {
 			throw new IllegalArgumentException("not valid dates for the program");
 	}
 	
-	public boolean datesCollideWithBooking(Date dateFrom, Date dateTo){//sjekker om datoer sendt inn kolliderer med denne bookingen
+	//sjekker om datoer sendt inn kolliderer med denne bookingen
+	public boolean datesCollideWithBooking(Date dateFrom, Date dateTo){
 		return !(dateFrom.isAfter(this.dateTo) || dateTo.isBefore(this.dateFrom));
 	}
 	
+	//samme som over
 	public boolean collideWith(BookingDate booking){
 		return datesCollideWithBooking(booking.dateFrom, booking.dateTo);
 	}
 	
+	//sjekker om en booking er etter denne
 	public boolean isAfter(BookingDate booking){
 		if (booking == null)
 			return false;
@@ -28,17 +31,19 @@ public class BookingDate {
 			
 	}
 	
+	//sjekker om en booking er lik denne
 	public boolean equals(BookingDate booking){
 		if (booking == null)
 			return false;
 		return dateFrom.equals(booking.dateFrom) && dateTo.equals(booking.dateTo);
 	}
 	
+	//samme som over
 	public boolean equals(Date dateFrom, Date dateTo){
 		return this.dateFrom.equals(dateFrom) && this.dateTo.equals(dateTo);
 	}
 	
 	public String toString(){
-		return "[DateFrom: " + dateFrom + ", DateTo: " + dateTo + ", Days: " + numberOfDays + "]";
+		return "[Fra: " + dateFrom + ", Til: " + dateTo + ", Antall dager: " + numberOfDays + "]";
 	}
 }
