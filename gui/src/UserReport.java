f47c5ecdc242a6e163d0aacd16d537eeed9116d0
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-06 13:54:25 +0200

    La til en JFrame i rapportklassen

--- gui/src/UserReport.java
+++ gui/src/UserReport.java
@@ -1,8 +1,16 @@
 package src;
 
+import javax.swing.JFrame;
+
 public class UserReport {
 	
+	private JFrame frame;
+	
 	public UserReport(){
-		
+		frame = new JFrame("Rapport");
+		frame.setSize(400, 400);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
