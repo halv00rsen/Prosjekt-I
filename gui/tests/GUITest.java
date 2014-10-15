f8748b180f23b1dc4691553206dbdcb0eccc2ae6
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-15 13:27:52 +0200

    Testing for adminloginklassen

--- gui/tests/GUITest.java
+++ gui/tests/GUITest.java
@@ -1,10 +1,31 @@
 package tests;
 
+import javax.swing.JFrame;
+
+import src.AdminLogin;
+import src.AdminLoginListener;
 import src.GUI;
 
-public class GUITest {
+public class GUITest implements AdminLoginListener{
 	
 	public static void main(String[] args){
-		GUI gui = new GUI();
+		GUITest test = new GUITest();
+		test.init();
+	}
+	
+	private void init(){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new AdminLogin(this));
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	public void login(String userName, char[] password) {
+		String pass = "";
+		for (char a : password)
+			pass += a;
+		System.out.println("Username: " + userName + ", passord: " + pass);
 	}
 }
