2ae6a04ff94010104432bc745ab758e5bebd995c
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-15 13:27:06 +0200

    Endret struktur

--- gui/src/GUI.java
+++ gui/src/GUI.java
@@ -12,35 +12,9 @@ import javax.swing.JPasswordField;
 public class GUI {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JPasswordField passwordField;
 	
 	public GUI(){
-		frame = new JFrame("NTNUI-Koiene");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500,500);
-		frame.getContentPane().setLayout(new GridLayout(3, 2, 0, 0));
-		
-		JLabel lblLogin = new JLabel("Login: ");
-		frame.getContentPane().add(lblLogin);
-		
-		textField = new JTextField();
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblPassord = new JLabel("Passord: ");
-		frame.getContentPane().add(lblPassord);
-		
-		passwordField = new JPasswordField();
-		frame.getContentPane().add(passwordField);
-		
-		JButton btnOk = new JButton("Ok");
-		frame.getContentPane().add(btnOk);
-		
-		JButton btnAvbryt = new JButton("Avbryt");
-		frame.getContentPane().add(btnAvbryt);
-		frame.pack();
-		frame.setVisible(true);
+		frame = new JFrame();
 		
 	}
 }
