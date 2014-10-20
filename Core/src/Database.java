b38f6d6ba4fd59e80378ec9535fcc069206ea1da
Aleksander Hansen <aleksanderhan@gmail.com>
2014-10-20 13:37:46 +0200

    lagd en konstruktør, begynt på en insert methode

--- Core/src/Database.java
+++ Core/src/Database.java
@@ -1,33 +1,47 @@
 package src;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
 	public static void main(String[] args) {
 		Database mySQL = new Database();
-		Connection conn = mySQL.connection();
 	}
 	
+	Connection conn;
 	
-	public static Connection connection() {
-		try {
+	public Database() {
+		this.conn = connection();
+	}
+	
+	public Connection connection() {
+		try {		
+			String url = "jdbc:mysql://mysql.stud.ntnu.no/";
+			String dbName = "alekh_prosjekt1";
+			String driver = "com.mysql.jdbc.Driver";
+			String userName = "alekh_IT1901";
+			String password = "abcd1234";
 				
-				String url = "jdbc:mysql://mysql.stud.ntnu.no/";
-				String dbName = "alekh_prosjekt1";
-				String driver = "com.mysql.jdbc.Driver";
-				String userName = "alekh_IT1901";
-				String password = "abcd1234";
-				
-				Class.forName(driver).newInstance();
-				Connection conn = DriverManager.getConnection(url+dbName,userName,password);
-				
-				conn.close();
+			Class.forName(driver).newInstance();
+			Connection conn = DriverManager.getConnection(url+dbName,userName,password);
 			
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		return null;
+			//conn.close();
+			return conn;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
+	
+	public void insert(String column, String value) {
+		try {
+			Statement st = conn.createStatement();
+			ResultSet res = st.executeQuery("SELECT * FROM event");
+			while (res.next()) {
+				String 
+				
+			//IKKE FERDIG...
+			}
+		}
+	}
+}
