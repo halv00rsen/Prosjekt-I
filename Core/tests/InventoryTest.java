569a8e6cdae338d97d0130fe9c98c72754018c1a
thomahan <thomasrenehansen@gmail.com>
2014-10-27 13:39:50 +0100

    Delte opp testen i flere deler.

--- Core/tests/InventoryTest.java
+++ Core/tests/InventoryTest.java
@@ -4,33 +4,36 @@ import src.Inventory;
 import src.Item;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 public class InventoryTest {
 	
 	@Test
-	public void testInstantiationAndSetBroken() {
+	public void testInstantiation() {
+		Inventory inventory = new Inventory();
+		assertTrue(inventory instanceof Inventory);
+	}
+	
+	@Test
+	public void testAddItemAndHasItem() {
 		Inventory inventory = new Inventory();
 		inventory.addItem("Gitar");
-		inventory.addItem("Vaffeljern");
 		
 		assertTrue(inventory.hasItem("Gitar"));
-		assertTrue(inventory.hasItem("Vaffeljern"));
-		
+	}
+	
+	@Test
+	public void testRemoveItem() {
+		Inventory inventory = new Inventory();
+		inventory.addItem("Gitar");
+
+		assertTrue(inventory.hasItem("Gitar"));
+
 		Item gitar = inventory.getItemByName("Gitar");
 		inventory.removeItem(gitar);
-		
+
 		assertFalse(inventory.hasItem("Gitar"));
-		
-		Item vaffeljern = inventory.getItemByName("Vaffeljern");
-		
-		assertFalse(vaffeljern.isBroken());
-		
-		vaffeljern.setBroken();
-		assertTrue(vaffeljern.isBroken());
 	}
 
 }
