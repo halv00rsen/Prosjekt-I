f732f12938758fb9eb377777f007b3015bf2db40
thomahan <thomasrenehansen@gmail.com>
2014-10-27 13:10:46 +0100

    Endret struktur, la til getItems-metode.

--- Core/src/Inventory.java
+++ Core/src/Inventory.java
@@ -10,6 +10,17 @@ public class Inventory {
 		items = new ArrayList<Item>();
 	}
 	
+	public void addItem(String name) {
+		items.add(new Item(name));
+		
+	}
+	
+	public void removeItem(Item item) {
+		if (items.contains(item)) {
+			items.remove(item);
+		}
+	}
+
 	public boolean hasItem(String name) {
 		for (Item item : items) {
 			if (item.getName().equals(name)) {
@@ -29,15 +40,7 @@ public class Inventory {
 		return null;
 	}
 	
-	public void addItem(String name) {
-		items.add(new Item(name));
-		
+	public List<Item> getItems() {
+		return items;
 	}
-	
-	public void removeItem(Item item) {
-		if (items.contains(item)) {
-			items.remove(item);
-		}
-	}
-
 }
