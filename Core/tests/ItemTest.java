9eefc82762f83018c308023e086cb6dd9b95cc62
thomahan <thomasrenehansen@gmail.com>
2014-10-27 13:51:33 +0100

    Delte testen opp i flere deler.

--- Core/tests/ItemTest.java
+++ Core/tests/ItemTest.java
@@ -1,30 +1,40 @@
 package tests;
 
 import src.Item;
+
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class ItemTest {
 	
 	@Test
-	public void testInstantiationAndSetBroken() {
-		
-		Item e = new Item("Gitar");
-		assertEquals(e.getName(), "Gitar");
-		
-		e.setName("El-gitar");
-		assertNotEquals(e.getName(), "Gitar");
-		assertEquals(e.getName(), "El-gitar");
-		
-		assertEquals(e.isBroken(), false);
-		
-		e.setBroken();
-		assertEquals(e.isBroken(), true);
-		
-		e.setBroken(false);
-		assertEquals(e.isBroken(), false);
-		
+	public void testInstantiation() {
+		Item item = new Item("Gitar");
+		assertTrue(item instanceof Item);
 	}
-
+	
+	@Test
+	public void testGetNameAndSetName() {
+		Item item = new Item("Gitar");
+		assertEquals(item.getName(), "Gitar");
+		
+		item.setName("El-gitar");
+		assertNotEquals(item.getName(), "Gitar");
+		assertEquals(item.getName(), "El-gitar");
+	}
+	
+	@Test
+	public void testIsBrokenAndSetBroken() {
+		Item item = new Item("Gitar");
+		assertFalse(item.isBroken());
+		
+		item.setBroken();
+		assertTrue(item.isBroken());
+		
+		item.setBroken(false);
+		assertFalse(item.isBroken());
+	}
 }
