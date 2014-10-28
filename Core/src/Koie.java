a5f7a97d65a7381dec54661937942ca71e98b7b8
thomahan <thomasrenehansen@gmail.com>
2014-10-28 21:34:33 +0100

    Byttet ut listen med Items med et Inventory-objekt

--- Core/src/Koie.java
+++ Core/src/Koie.java
@@ -1,21 +1,20 @@
 package src;
 
-import java.util.List;
-
+import src.Inventory;
 
 public class Koie {
 	private final String name;
-	private final String id, numBeds, numSeats, year; //trenger ikke å være int fordi vi bruker aldri de numeriske verdiene til noe
+	private final String id, numBeds, numSeats, year; //trenger ikke ï¿½ vï¿½re int fordi vi bruker aldri de numeriske verdiene til noe
 	private final boolean sykkel, toppTur, jakt, fiske;
 	private final String coordinates, terreng, spesialiteter;
-	private final List<Item> inventory;
+	private final Inventory inventory;
 	private final Calendar cabinRented;
 
 	public Koie(String id, String name,
 				String numBeds, String numSeats, String coordinates,
 				String year, String terreng,
 				boolean sykkel, boolean toppTur, boolean jakt, boolean fiske,
-				String spesialiteter, List<Item> inventory) {
+				String spesialiteter, Inventory inventory) {
 
 		this.id = id;
 		this.name = name;
@@ -33,21 +32,6 @@ public class Koie {
 			
 		cabinRented = new Calendar();
 	}
-	
-	public String toString() {
-		return name +"\t"+
-				String.valueOf(numBeds) +"\t"+
-				String.valueOf(numBeds) +"\t"+
-				coordinates +"\t"+
-				String.valueOf(year) +"\t"+
-				terreng +"\t"+
-				String.valueOf(sykkel) +"\t"+
-				String.valueOf(toppTur) +"\t"+
-				String.valueOf(jakt) +"\t"+
-				String.valueOf(fiske) +"\t"+
-				spesialiteter;
-	}
-			
 
 	public String getId() {
 		return id;
@@ -93,15 +77,29 @@ public class Koie {
 		return spesialiteter;
 	}
 	
-	public List<Item> getEquipment() {
+	public Inventory getInventory() {
 		return inventory;
 	}
 
-	public void reserveCabin(int dayFrom, int monthFrom, int numOfDays, String name){
-		if (cabinRented.validDate(dayFrom,monthFrom)) {
+//	public void reserveCabin(int dayFrom, int monthFrom, int numOfDays, String name){
+//		if (cabinRented.validDate(dayFrom,monthFrom)) {
 //			cabinRented.reservePeriod( dayFrom, monthFrom, numOfDays)
-			}
+//			}
 //		else
+//	}
+
+	public String toString() {
+		return name +"\t"+
+				String.valueOf(numBeds) +"\t"+
+				String.valueOf(numBeds) +"\t"+
+				coordinates +"\t"+
+				String.valueOf(year) +"\t"+
+				terreng +"\t"+
+				String.valueOf(sykkel) +"\t"+
+				String.valueOf(toppTur) +"\t"+
+				String.valueOf(jakt) +"\t"+
+				String.valueOf(fiske) +"\t"+
+				spesialiteter;
 	}
 }
 	
