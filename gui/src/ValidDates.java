e8f8bde81ebe4299062b5ff48a2152a4144f254d
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-29 16:30:03 +0100

    Kan nå hente dataene som er lagret i validDates

--- gui/src/ValidDates.java
+++ gui/src/ValidDates.java
@@ -11,39 +11,56 @@ import javax.swing.JPanel;
 public class ValidDates extends JPanel{
 
 	private final Date today, maxDate;
-	private final JComboBox<Integer> date, month;
+	private final JComboBox<Integer> date, numDays;
+	private final JComboBox<Months> month;
 	
 	public ValidDates(){
 		Calendar calendar = new Calendar();
 		today = calendar.getTodaysDate();
 		maxDate = calendar.getMaxDate();
 		date = new JComboBox<Integer>();
-		month = new JComboBox<Integer>();
+		month = new JComboBox<Months>();
+		numDays = new JComboBox<Integer>();
+		for (int a = 1; a < 11; a++){
+			numDays.addItem(a);
+		}
 		month.addActionListener(new MonthChanged());
 		int month = today.month;
-		for (int a = month; a < month + 6; a++){
+		for (int a = month; a <= month + 6; a++){
 			if (a % 12 == 0)
-				this.month.addItem(12);
+				this.month.addItem(Months.getMonth(12));
 			else
-				this.month.addItem(a % 12);
+				this.month.addItem(Months.getMonth(a % 12));
 		}
 		updateDays();
-		this.setLayout(new GridLayout(1, 4));
+		this.setLayout(new GridLayout(1, 6));
 		this.add(new JLabel("Dag: "));
 		this.add(date);
 		this.add(new JLabel("Måned: "));
 		this.add(this.month);
+		this.add(new JLabel("Antall dager: "));
+		this.add(numDays);
+		
+	}
+	
+	public int[] getReservation(){//returns [dag, måned, antall dager]
+		int[] res = new int[3];
+		res[0] = (int) date.getSelectedItem();
+		res[1] = ((Months) month.getSelectedItem()).getValue();
+		res[2] = (int) numDays.getSelectedItem();
+		return res;
 	}
 	
 	private void updateDays(){
 		date.removeAllItems();
-		int valueMonth = (int) month.getSelectedItem();
+		int valueMonth = ((Months) month.getSelectedItem()).getValue();
 		int start = 1;
 		int end = Calendar.getDayOfMonth(valueMonth);
 		if (valueMonth == today.month){
 			start = today.day;
-		}else if (valueMonth == maxDate.month)
+		}else if (valueMonth == maxDate.month){
 			end = maxDate.day;
+		}
 		for (int a = start; a < end + 1; a++){
 			date.addItem(a);
 		}
