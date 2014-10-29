922d9ae63ca6d9c0215ddcb6f4ca873f7fe11053
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-29 16:29:37 +0100

    Flyttet noe av jobben til andre klasser

--- gui/src/GUI.java
+++ gui/src/GUI.java
@@ -18,21 +18,20 @@ import javax.swing.JComboBox;
 public class GUI {
 
 	private JFrame frame;
-	private AdminLogin loginUser;
-	private ValidDates validDates;
+	
+	private LoginPanel loginPanel;
+	private ReservationsFrame reservationFrame;
 	
 	public GUI(){
-		frame = new JFrame();
+		frame = new JFrame("NTNUI-Koiene");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500, 500);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
-		loginUser = new AdminLogin(new Login());
-		JPanel login = new JPanel();
-		tabbedPane.addTab("Innlogging", null, login, null);
-		login.add(loginUser);
+		loginPanel = new LoginPanel();
+		tabbedPane.addTab("Innlogging", null, loginPanel, null);
 		
 		
 		JPanel Koier = new JPanel();
@@ -42,39 +41,15 @@ public class GUI {
 		tabbedPane.addTab("Reservasjoner", null, reservations, null);
 		
 		
-		JPanel reserveCabin = new JPanel();
-		tabbedPane.addTab("Reserver Koie", null, reserveCabin, null);
-		
-		reserveCabin.setLayout(new GridLayout(4, 4));
-		reserveCabin.add(new JLabel("Velg Koie: "));
-		JComboBox<String> comboBox = new JComboBox<String>();
-		reserveCabin.add(comboBox);
-		for (String name : getNameCabins())
-			comboBox.addItem(name);
-		reserveCabin.add(new JLabel("Velg dato: "));
-		validDates = new ValidDates();
-		reserveCabin.add(validDates);
+		reservationFrame = new ReservationsFrame();
+		tabbedPane.addTab("Reserver Koie", null, reservationFrame, null);
+		loginPanel.addListener(reservationFrame);
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Kart", null, panel_2, null);
+		frame.pack();
 		frame.setVisible(true);
 		
 	}
 	
-	private List<String> getNameCabins(){
-		List<String> koie = new ArrayList<String>();
-		koie.add("Koie 1");
-		koie.add("Koie 2");
-		koie.add("Koie 3");
-		koie.add("Koie 4");
-		return koie;
-	}
-	
-	private class Login implements AdminLoginListener{
-
-		public void login(String userName, char[] password) {
-			System.out.println(userName);
-		}
-		
-	}
 }
