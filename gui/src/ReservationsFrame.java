963bc2652b264002ce3655bc912ff64ff950d96d
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-10-30 22:16:36 +0100

    Endret fullstendig oppsett, nå ser siden ryddig ut

--- gui/src/ReservationsFrame.java
+++ gui/src/ReservationsFrame.java
@@ -1,5 +1,8 @@
 package src;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,43 +10,84 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class ReservationsFrame extends JPanel implements LoginListener, ValidDatesListener{
 	
 	private ValidDates validDates;
 	private JButton reserveButton;
 	private JComboBox<String> cabins;
+	private JComboBox<Integer> day, numDays;
+	private JComboBox<Months> month;
 	private boolean isLoggedIn;
 	private String username;
 	private final JLabel isValidDateReservation;
+	private final JTextArea cabinInformation;
 	
 	public ReservationsFrame(){
-		setLayout(new GridLayout(4, 4));
-		add(new JLabel("Velg Koie: "));
+		setLayout(new GridLayout(1,2));
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		JPanel panel = new JPanel();
+		panel.setLayout(layout);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.1;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(new JLabel("Velg Koie: "), c);
 		cabins = new JComboBox<String>();
-		add(cabins);
+		c.gridx = 3;
+		panel.add(cabins, c);
 		for (String name : getNameCabins())
 			cabins.addItem(name);
-		add(new JLabel("Velg dato: "));
-		validDates = new ValidDates();
-		add(validDates);
-		reserveButton = new JButton("Reserver");
-		reserveButton.addActionListener(new ButtonListener());
-		add(reserveButton);
+		c.gridy = 1;
+		c.gridx = 0;
+		panel.add(new JLabel("Velg dag: "), c);
+		day = new JComboBox<Integer>();
+		month = new JComboBox<Months>();
+		numDays = new JComboBox<Integer>();
+		validDates = new ValidDates(day, month, numDays);
+		cabins.addActionListener(new CabinListener());
+		c.gridx = 3;
+		panel.add(day, c);
+		c.gridy = 2;
+		panel.add(month, c);
+		c.gridx = 0;
+		panel.add(new JLabel("Velg måned:"), c);
+		c.gridy = 3;
+		panel.add(new JLabel("Velg antall dager:"), c);
+		c.gridx = 3;
+		panel.add(numDays, c);
 		isLoggedIn = false;
 		username = null;
-		isValidDateReservation = new JLabel("Er ledig: ");
-		add(isValidDateReservation);
+		c.gridy = 5;
+		c.gridx = 0;
+		panel.add(new JLabel("Er ledig:"), c);
+		c.gridx = 3;
+		isValidDateReservation = new JLabel("-");
+		panel.add(isValidDateReservation, c);
+		reserveButton = new JButton("Reserver");
+		reserveButton.addActionListener(new ButtonListener());
+		c.gridy = 6;
+		c.gridx = 2;
+		panel.add(reserveButton, c);
 		validDates.addListener(this);
+		add(panel);
+		cabinInformation = new JTextArea(4, 4);
+		cabinInformation.setEditable(false);
+		cabinInformation.setBorder(BorderFactory.createEtchedBorder());
+		add(cabinInformation);
 	}
 	
 	public void isValidDate(boolean isValid){
-		isValidDateReservation.setText("Er ledig: " + (isValid ? "Ja": "Nei"));
+		isValidDateReservation.setForeground(isValid ? Color.green: Color.red);
+		isValidDateReservation.setText((isValid ? "Ja": "Nei"));
 	}
 	
 	private boolean isValidReservation(int day, int month, int numDays){
@@ -98,6 +142,12 @@ public class ReservationsFrame extends JPanel implements LoginListener, ValidDat
 		public void actionPerformed(ActionEvent e) {
 			buttonPressed();
 		}
+		
+	}
+
+	@Override
+	public void adminHasLoggedIn() {
+		// TODO Auto-generated method stub
 		
 	}
 }
