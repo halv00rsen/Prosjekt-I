8a46e98ae796cca30a0413d4f1704560b101fc21
thomahan <thomasrenehansen@gmail.com>
2014-11-04 16:09:06 +0100

    Fjernet utkommenterte linjer og utdaterte imports

--- Core/src/Main.java
+++ Core/src/Main.java
@@ -1,33 +1,18 @@
 package src;
 
-import java.util.Scanner;
-import java.io.FileReader;
-
 public class Main {
-	
-	private textGUI textGui;
-	private Database database;
+	private static Database database;
+	//private textGUI textGui;
 	
 	private void init(){
-		//textGui = new textGUI();
-		//textGui.Alternativer("bruker");
-//		database = new Database();
-		
-		//legger til koieinfo fra "initialiseringAvKoier.dat"
-//		Scanner in = new Scanner("initialiseringAvKoier.dat");
-//		String[] kolonner = in.nextLine().split(" ");
-//		for (String elem : kolonner) {
-//			System.out.println(elem);
-//		}
-//		in.close();
-		//textGui = new textGUI();
-		//textGui.Alternativer("bruker");
 		database = new Database();
 		database.initializeDatabase("Core/src/initialiseringAvKoier.dat");
+
+		//textGui = new textGUI();
+		//textGui.Alternativer("bruker");
 	}
 	
 	private void run(){
-		
 	}
 	
 	public static void main(String[] args){
@@ -35,6 +20,4 @@ public class Main {
 		program.init();
 		program.run();
 	}
-
 }
-
