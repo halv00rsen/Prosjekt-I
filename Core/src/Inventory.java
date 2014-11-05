d4a9a491b5757a013b1f138b1d06e7de30bc142c
thomahan <thomasrenehansen@gmail.com>
2014-11-05 15:43:49 +0100

    La til javadoc-kommentarer for alle metoder, fjernet getItemByName og hasItemById

--- Core/src/Inventory.java
+++ Core/src/Inventory.java
@@ -6,24 +6,35 @@ import java.util.ArrayList;
 public class Inventory {
 	private List<Item> items;
 	
+	/**
+	 * Oppretter tomt Inventory-objekt
+	 */
 	public Inventory() {
 		items = new ArrayList<Item>();
 	}
 	
 	/**
-	 * Adds item to inventory
-	 * @param item Item to be added to inventory
+	 * Legger til et Item-objekt
+	 * @param item Item-objekt
 	 */
 	public void addItem(Item item) {
 		items.add(item);
 	}
-	
+
+	/**
+	 * Fjerner et Item-objekt
+	 * @param item Item-objekt
+	 */
 	public void removeItem(Item item) {
 		if (items.contains(item)) {
 			items.remove(item);
 		}
 	}
 
+	/**
+	 * Returnerer alle Item-objekter
+	 * @return Liste med Item-objekter
+	 */
 	public List<Item> getAllItems() {
 		List<Item> allItems = new ArrayList<Item>();
 		for (Item item : items) {
@@ -32,6 +43,10 @@ public class Inventory {
 		return allItems;
 	}
 
+	/**
+	 * Returnerer alle Item-objekter som er i orden
+	 * @return Liste med Item-objekter
+	 */
 	public List<Item> getInOrderItems() {
 		List<Item> inOrderItems = new ArrayList<Item>();
 		for (Item item : items) {
@@ -42,6 +57,10 @@ public class Inventory {
 		return inOrderItems;
 	}
 
+	/**
+	 * Returnerer alle Item-objekter som er ødelagte
+	 * @return Liste med Item-objekter
+	 */
 	public List<Item> getBrokenItems() {
 		List<Item> brokenItems = new ArrayList<Item>();
 		for (Item item : items) {
@@ -52,6 +71,10 @@ public class Inventory {
 		return brokenItems;
 	}
 
+	/**
+	 * Returnerer alle Item-objekter som er gjenglemte
+	 * @return Liste med Item-objekter
+	 */
 	public List<Item> getLostAndFoundItems() {
 		List<Item> lostAndFoundItems = new ArrayList<Item>();
 		for (Item item : items) {
@@ -62,6 +85,10 @@ public class Inventory {
 		return lostAndFoundItems;
 	}
 
+	/**
+	 * Returnerer alle Item-objekter som nylig lagt til (dvs. ikke har fått en unik ID i databasen enda).
+	 * @return Liste med Item-objekter
+	 */
 	public List<Item> getNewItems() {
 		List<Item> newItems = new ArrayList<Item>();
 		for (Item item : items) {
@@ -72,6 +99,11 @@ public class Inventory {
 		return newItems;
 	}
 
+	/**
+	 * Returnerer et Item-objekt med den gitte ID-en
+	 * @param id
+	 * @return Item-objekt
+	 */
 	public Item getItemById(int id) {
 		for (Item item : items) {
 			if (item.getId() == id) {
@@ -79,23 +111,5 @@ public class Inventory {
 			}
 		}
 		return null;
-	}
-
-	public Item getItemByName(String name) {
-		for (Item item : items) {
-			if (item.getName().equals(name)) {
-				return item;
-			}
-		}
-		return null;
-	}
-
-	public boolean hasItemByName(String name) {
-		for (Item item : items) {
-			if (item.getName().equals(name)) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
