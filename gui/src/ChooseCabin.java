11f4d0ad1ee18152691a4984292be5bb9b470615
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-07 10:15:28 +0100

    Kommuniserer nå med databasen, henter ut navn og id til hver koie

--- gui/src/ChooseCabin.java
+++ gui/src/ChooseCabin.java
@@ -2,6 +2,7 @@ package src;
 
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JComboBox;
@@ -9,9 +10,11 @@ import javax.swing.JComboBox;
 public class ChooseCabin {
 
 	private JComboBox<String> cabins;
+	private HashMap<Integer, String> cabinsIdName;
 	
 	public ChooseCabin(){
 		cabins = new JComboBox<String>();
+		cabinsIdName = Database.getIdNameMap();
 		for (String name : getNameCabins())
 			cabins.addItem(name);
 	}
@@ -22,16 +25,27 @@ public class ChooseCabin {
 	
 //	Skal kommunisere med databasen
 	private List<String> getNameCabins(){
-		List<String> koie = new ArrayList<String>();
-		koie.add("Koie 1");
-		koie.add("Koie 2");
-		koie.add("Koie 3");
-		koie.add("Koie 4");
-		return koie;
+		List<String> cabinNames = new ArrayList<String>();
+		for (Integer id : cabinsIdName.keySet()){
+			cabinNames.add(cabinsIdName.get(id));
+		}
+		return cabinNames;
 	}
 	
-	public String getSelectedItem(){
-		return (String) cabins.getSelectedItem();
+	public String getCabinWith(int id){
+		return cabinsIdName.get(id);
+	}
+	
+	public int getSelectedItem(){
+		String item = (String) cabins.getSelectedItem();
+		int cabinId = -1;
+		for (Integer id : cabinsIdName.keySet()){
+			if (cabinsIdName.get(id).equals(item)){
+				cabinId = id;
+				break;
+			}
+		}
+		return cabinId;
 	}
 	
 	public void addActionListener(ActionListener l){
