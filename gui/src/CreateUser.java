f73d047894029efbb1aafabd1c666c253a3e082d
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-10 17:20:06 +0100

    Skrevet javadockode

--- gui/src/CreateUser.java
+++ gui/src/CreateUser.java
@@ -12,12 +12,15 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class CreateUser extends JPanel{
-	
 	private final JTextField username;
 	private final JPasswordField password, checkPassword;
 	private final JButton ok, cancel;
 	public final static int leastLengthPassword = 6;
-	
+
+	/**
+	 * Oppretter objektet der man kan lage en ny bruker i systemet.
+	 * 
+	 */
 	public CreateUser(){
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(4, 2, 0, 0));
@@ -40,6 +43,10 @@ public class CreateUser extends JPanel{
 		add(panel);
 	}
 
+	/**
+	 * Sjekker om emailen er en valid emailadresse og om den finnes fra før i databasen
+	 * @return boolean 
+	 */
 	private boolean validEmail(){
 		String email = username.getText().toLowerCase().trim();
 		if (email.indexOf("@") == -1 || email.indexOf(".") == -1){
@@ -50,6 +57,10 @@ public class CreateUser extends JPanel{
 		return true;
 	}
 	
+	/**
+	 * Sjekker om passordene stemmer overens og om det er lang nok lengde på dem
+	 * @return boolean
+	 */
 	private boolean validPassword(){
 		char[] password1 = password.getPassword();
 		if (password1.length < leastLengthPassword){
@@ -71,6 +82,9 @@ public class CreateUser extends JPanel{
 		
 	}
 	
+	/**
+	 * Fjerner all tekst i alle inputfelt på siden
+	 */
 	public void resetFields(){
 		username.setText("");
 		password.setText("");
@@ -78,7 +92,10 @@ public class CreateUser extends JPanel{
 	}
 	
 	private class ButtonListener implements ActionListener{
-
+		
+		/**
+		 * @param arg0 en actionevent som knappene implementerer
+		 */
 		public void actionPerformed(ActionEvent arg0) {
 			if (arg0.getSource() == ok){
 				if (!validPassword() || !validEmail())
