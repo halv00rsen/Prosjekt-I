93e70403aeb5772318b836f40be0e8080b0dd355
thomahan <thomasrenehansen@gmail.com>
2014-11-11 17:45:01 +0100

    Endret etter refaktorering av Inventory-klassen

--- Core/tests/InventoryTest.java
+++ Core/tests/InventoryTest.java
@@ -1,37 +1,14 @@
 package tests;
 
 import src.Inventory;
-import src.Item;
 
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 public class InventoryTest {
 	@Test
 	public void testInstantiation() {
 		Inventory inventory = new Inventory();
 		assertTrue(inventory instanceof Inventory);
-	}
-	
-	@Test
-	public void testAddItemAndHasItem() {
-		Inventory inventory = new Inventory();
-		inventory.addItemByName("Gitar");
-		
-		assertTrue(inventory.hasItemByName("Gitar"));
-	}
-	
-	@Test
-	public void testRemoveItem() {
-		Inventory inventory = new Inventory();
-		inventory.addItemByName("Gitar");
-
-		assertTrue(inventory.hasItemByName("Gitar"));
-
-		Item gitar = inventory.getItemByName("Gitar");
-		inventory.removeItem(gitar);
-
-		assertFalse(inventory.hasItemByName("Gitar"));
 	}
 }
