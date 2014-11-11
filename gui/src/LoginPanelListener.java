5db18d32ff38d24bddfebe57ab08288f027e67ab
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-11 19:04:34 +0100

    Slettet en unødvending listener

--- gui/src/LoginPanelListener.java
+++
@@ -1,6 +0,0 @@
-package src;
-
-public interface LoginPanelListener {
-	public boolean isValidAdminLogin(String username, char[] password);
-	public boolean isValidLogin(String username, char[] password);
-}
