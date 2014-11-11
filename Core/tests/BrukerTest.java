7fa67c88f75251be9248aee3878d4648143e7891
thomahan <thomasrenehansen@gmail.com>
2014-11-11 17:41:21 +0100

    Endret etter refaktorering av Bruker-klassen

--- Core/tests/BrukerTest.java
+++ Core/tests/BrukerTest.java
@@ -2,15 +2,14 @@ package tests;
 
 import src.Bruker;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 
 public class BrukerTest {
 	@Test
 	public void instanceTest() {
-		Bruker bruker = new Bruker("admin1", Bruker.hashPassword("password"), Bruker.Status.ADMIN);
+		Bruker bruker = new Bruker("admin1", Bruker.hashPassword("password"), true);
 		System.out.println(bruker.getId());
 		System.out.println(bruker.getPasswordHash());
-		System.out.println(bruker.getStatus());
+		System.out.println(bruker.isAdmin());
 		System.out.println("password");
 		System.out.println(Bruker.hashPassword("password"));
 		System.out.println(bruker.isPasswordCorrect("password"));
