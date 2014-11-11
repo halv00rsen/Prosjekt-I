0d5e8def3e0862d8efdc1d539cb52a18c638266c
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-11 19:05:26 +0100

    Støtter nå å lage ny adminbruker, og små endringer uteseendemessig

--- gui/src/CreateUser.java
+++ gui/src/CreateUser.java
@@ -1,10 +1,14 @@
 package src;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -13,30 +17,53 @@ import javax.swing.JTextField;
 
 public class CreateUser extends JPanel{
 	private final JTextField username;
-	private final JPasswordField password, checkPassword;
+	private final JPasswordField password, checkPassword, adminCode;
 	private final JButton ok, cancel;
 	public final static int leastLengthPassword = 6;
+	private final JCheckBox adminCheckBox;
+	private final GridBagConstraints c;
+	private final JPanel panel;
 
 	/**
 	 * Oppretter objektet der man kan lage en ny bruker i systemet.
 	 * 
 	 */
 	public CreateUser(){
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(4, 2, 0, 0));
-		panel.add(new JLabel("Email: "));
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createTitledBorder("Ny bruker"));
+		adminCheckBox = new JCheckBox();
+		adminCheckBox.addActionListener(new AdminListener());
+		adminCode = new JPasswordField(4);
+		c = new GridBagConstraints();
+		panel.setLayout(new GridBagLayout());
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(new JLabel("Email: "), c);
 		username = new JTextField(20);
-		panel.add(username);
-		panel.add(new JLabel("Passord: "));
-		password = new JPasswordField();
-		panel.add(password);
-		panel.add(new JLabel("Gjenta passord: "));
-		checkPassword = new JPasswordField();
-		panel.add(checkPassword);
+		c.gridx = 1;
+		panel.add(username, c);
+		c.gridy = 1;
+		password = new JPasswordField(20);
+		panel.add(password, c);
+		c.gridx = 0;
+		panel.add(new JLabel("Passord: "), c);
+		c.gridy = 2;
+		panel.add(new JLabel("Gjenta passord: "), c);
+		checkPassword = new JPasswordField(20);
+		c.gridx = 1;
+		panel.add(checkPassword, c);
 		ok = new JButton("Lag bruker");
-		panel.add(ok);
+		c.gridy = 3;
+		c.gridx = 0;
+		panel.add(ok, c);
 		cancel = new JButton("Avbryt");
-		panel.add(cancel);
+		c.gridx = 1;
+		panel.add(cancel,c);
+		c.gridy = 2;
+		c.gridx = 2;
+		panel.add(new JLabel("Adminbruker: "), c);
+		c.gridx = 3;
+		panel.add(adminCheckBox, c);
 		ButtonListener listener = new ButtonListener();
 		ok.addActionListener(listener);
 		cancel.addActionListener(listener);
@@ -89,6 +116,29 @@ public class CreateUser extends JPanel{
 		username.setText("");
 		password.setText("");
 		checkPassword.setText("");
+	}
+	
+	private class AdminListener implements ActionListener{
+		
+		private JLabel adminKode;
+		
+		private AdminListener(){
+			adminKode = new JLabel("Adminkode: ");
+		}
+		
+		public void actionPerformed(ActionEvent arg0){
+			if (adminCheckBox.isSelected()){
+				c.gridx = 2;
+				c.gridy = 0;
+				panel.add(adminKode, c);
+				c.gridy = 1;
+				panel.add(adminCode, c);
+			}else{
+				panel.remove(adminCode);
+				panel.remove(adminKode);
+			}
+			panel.repaint();
+		}
 	}
 	
 	private class ButtonListener implements ActionListener{
