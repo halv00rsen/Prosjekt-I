7af190ea97be2bcfdb81613de2b23b644516c032
thomahan <thomasrenehansen@gmail.com>
2014-11-12 17:04:02 +0100

    La til klasse-beskrivelse, omgrupperte metoder

--- Core/src/Koie.java
+++ Core/src/Koie.java
@@ -1,28 +1,24 @@
 package src;
 
-import src.Inventory;
-
+/** Inneholder informasjon, utstyr og reservasjoner knyttet til en koie */
 public class Koie {
 	private final int id;
 	private final String name;
 	private final String coordinate;
 	private final int year;
-	
+	private final Inventory inventory;
+	private final Calendar cabinRented;
+
 	private int numBeds;
 	private int numSeats;
+	private double vedmengde;
 	private String terreng;
 	private String sykkel;
 	private String topptur;
 	private String jaktOgFiske;
 	private String spesialiteter;
-	
-	private final Inventory inventory;
-	private final Calendar cabinRented;
-	private double vedmengde;
-
 
 	public Koie(int id, String name, String coordinate, int year) {
-
 		this.id = id;
 		this.name = name;
 		this.coordinate = coordinate;
@@ -47,81 +43,79 @@ public class Koie {
 	public int getYear() {
 		return year;
 	}
-	
-	public double getVedmengde() {
-		return vedmengde;
-	}
-	
-	public int getNumBeds() {
-		return numBeds;
-	}
-	
-	public int getNumSeats() {
-		return numSeats;
-	}
-	
-	public String getTerreng() {
-		return terreng;
-	}
-	
-	public String getSykkel() {
-		return sykkel;
-	}
-	
-	public String getTopptur() {
-		return topptur;
-	}
-	
-	public String getJaktOgFiske() {
-		return jaktOgFiske;
-	}
-	
-	public String getSpesialiteter() {
-		return spesialiteter;
+		
+	public Inventory getInventory() {
+		return inventory;
 	}
 	
 	public Calendar getCalendar() {
 		return cabinRented;
 	}
 	
-	public Inventory getInventory() {
-		return inventory;
-	}
-	
-	
-	
-	public void setVedmengde(double ved) {
-		this.vedmengde = ved;
+	public int getNumBeds() {
+		return numBeds;
 	}
 	
 	public void setNumBeds(int beds) {
 		this.numBeds = beds;
 	}
-	
+
+	public int getNumSeats() {
+		return numSeats;
+	}
+		
 	public void setNumSeats(int seats) {
 		this.numSeats = seats;
+	}
+	
+	public double getVedmengde() {
+		return vedmengde;
+	}
+		
+	public void setVedmengde(double ved) {
+		this.vedmengde = ved;
+	}
+
+	public String getTerreng() {
+		return terreng;
 	}
 	
 	public void setTerreng(String terreng) {
 		this.terreng = terreng;
 	}
-	
+
+	public String getSykkel() {
+		return sykkel;
+	}
+		
 	public void setSykkel(String sykkel) {
 		this.sykkel = sykkel;
+	}
+	
+	public String getTopptur() {
+		return topptur;
 	}
 	
 	public void setTopptur(String topptur) {
 		this.topptur = topptur;
 	}
-	
+
+	public String getJaktOgFiske() {
+		return jaktOgFiske;
+	}
+		
 	public void setJaktOgFiske(String jaktofisk) {
 		this.jaktOgFiske = jaktofisk;
 	}
-	
+
+	public String getSpesialiteter() {
+		return spesialiteter;
+	}
+		
 	public void setSpesialiteter(String spesial) {
 		this.spesialiteter = spesial;
 	}
-	
+
 	/*
 	public String toString() {
 		return name +"\t"+
