5052a9ccdb8a9764f413fe8a535448af43443ead
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-12 16:45:08 +0100

    Admin får nå inn alle reservasjoner på en bestemt koie og kan slette dem

--- gui/src/RemoveReservationsAdmin.java
+++ gui/src/RemoveReservationsAdmin.java
@@ -4,17 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class RemoveReservationsAdmin extends JPanel implements ReservationRowListener{
 
-	private JPanel panel;
-	private ChooseCabin chooseCabin;
+	private final JPanel panel;
+	private final ChooseCabin chooseCabin;
+	private Koie cabin;
+	private final ReservationsListListener deleteListener;
 	
-	public RemoveReservationsAdmin(){
+	public RemoveReservationsAdmin(ReservationsListListener deleteListener){
 		panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
 		chooseCabin = new ChooseCabin();
@@ -23,21 +29,39 @@ public class RemoveReservationsAdmin extends JPanel implements ReservationRowLis
 		panel2.add(new JLabel("Koie: "));
 		panel2.add(chooseCabin.getComboBox(), BorderLayout.EAST);
 		JScrollPane pane = new JScrollPane(panel);
+		chooseCabin.addActionListener(new KoieListener());
+		panel.setBorder(BorderFactory.createTitledBorder("Reservasjoner"));
 		pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		getAllReservations();
 		add(panel2);
 		add(pane);
+		this.deleteListener = deleteListener;
 	}
 	
 	private void getAllReservations(){
+		panel.removeAll();
+		cabin = Database.getKoie(chooseCabin.getSelectedItem());
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
-		panel.add(new ReservationRow("Kallekula", new Date(13, 11), new Date(18, 11),new Date(1,1), false, true, this), c);
+		for (BookingDate booking: cabin.getCalendar().getDatesBooked()){
+			panel.add(new ReservationRow(cabin.getName(),booking.person, booking.dateFrom, booking.dateTo, cabin.getCalendar().getTodaysDate(), true, true, this), c);
+			c.gridy++;
+		}
 	}
 
 	public void removeReservation(ReservationRow reservation) {
 		panel.remove(reservation);
+		deleteListener.removeReservation(reservation.getFrom(), reservation.getTo(), reservation.getCabinName());
+		panel.revalidate();
+		panel.repaint();
+	}
+	
+	private class KoieListener implements ActionListener{
+
+		public void actionPerformed(ActionEvent arg0) {
+			getAllReservations();
+		}
 	}
 }
