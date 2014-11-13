74eb452538ad29e7194248d6d30569505689528f
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-13 14:37:48 +0100

    Flyttet sortedlistmodel ut av klassen

--- gui/src/DestroyedItems.java
+++ gui/src/DestroyedItems.java
@@ -145,44 +145,4 @@ public class DestroyedItems extends JPanel{
 			  clearBrokenInventorySelected();
 		  }
 	  }
-
-	/**
-	 * 
-	 * Klassen håndterer elementene i listene
-	 */
-	private class SortedListModel extends AbstractListModel<Item> {
-	
-		  private SortedSet<Item> model;
-	
-		  public SortedListModel() {
-			  model = new TreeSet<Item>();
-		  }
-	
-		  public int getSize() {
-			  return model.size();
-		  }
-	
-		  public Item getElementAt(int index) {
-			  return (Item) model.toArray()[index];
-		  }
-	
-		  public void add(Item element) {
-			  if (model.add(element)) {
-				  fireContentsChanged(this, 0, getSize());
-			  }
-		  }
-	
-		  public void clear() {
-			  model.clear();
-			  fireContentsChanged(this, 0, getSize());
-		  }
-	
-		  public boolean removeElement(Item element) {
-			  boolean removed = model.remove(element);
-			  if (removed) {
-				  fireContentsChanged(this, 0, getSize());
-			  }
-			  return removed;
-		  }
-	}
 }
