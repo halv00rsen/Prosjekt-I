68a236150d84ae75d07ab260f121630e2555edfd
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-13 17:20:04 +0100

    Admin kan nå se utstyrsstatus til alle koiene

--- gui/src/ItemStatus.java
+++ gui/src/ItemStatus.java
@@ -9,35 +9,59 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class ItemStatus extends JPanel{
 
-	private ChooseCabin cabins;
-	private JTextArea itemInformation;
+	private final ChooseCabin cabins;
+	private final JTextArea inOrder, broken, lostAndFound;
 	
 	public ItemStatus(){
 		cabins = new ChooseCabin();
 		cabins.addActionListener(new CabinListener());
+//		panel.add(cabins.getComboBox(), c);
 		setLayout(new GridLayout(2,1));
+		add(cabins.getComboBox());
+		
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 0;
-		panel.add(new JLabel("Velg koie: "), c);
-		c.gridx = 2;
-		panel.add(cabins.getComboBox(), c);
-		itemInformation = new JTextArea();
-		itemInformation.setEditable(false);
-		itemInformation.setBorder(BorderFactory.createEtchedBorder());
+		panel.setLayout(new GridLayout(1,3));
+		inOrder = createTextArea("Utstyr", panel);
+		broken = createTextArea("Ødelagt", panel);
+		lostAndFound = createTextArea("Mistet utstyr", panel);
 		add(panel);
-		add(itemInformation);
+		
 		setKoieInformation(cabins.getSelectedItem());
+		
 	}
 	
-	private void setKoieInformation(int cabin){
-		itemInformation.setText(cabin + "\nSomething cool.");
+	private JTextArea createTextArea(String header, JPanel panel){
+		JTextArea area = new JTextArea();
+		area.setEditable(false);
+		area.setBorder(BorderFactory.createTitledBorder(null, header));
+		JScrollPane pane = new JScrollPane(area);
+		pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		panel.add(pane);
+		return area;
+	}
+	
+	private void setKoieInformation(int cabinId){
+		Inventory cabin = Database.getKoie(cabinId).getInventory();
+		String inOrder = "";
+		for (Item item : cabin.getInOrderItems())
+			inOrder += item.getName() + "\n";
+		this.inOrder.setText(inOrder);
+		String broken = "";
+		for (Item item : cabin.getBrokenItems())
+			broken += item.getName() + "\n";
+		this.broken.setText(broken);
+		String lost = "";
+		for (Item item: cabin.getLostAndFoundItems())
+			lost += item.getName() + "\n";
+		this.lostAndFound.setText(lost);
+		this.revalidate();
+		this.repaint();
 	}
 	
 	private class CabinListener implements ActionListener{
