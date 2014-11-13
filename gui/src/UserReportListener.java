552513bb942e50fd36790df2fb063f2c96f7188d
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-13 14:39:48 +0100

    Endret metoden okPressed, tar nå inn brukte vedsekker og gjenglemt utstyr

--- gui/src/UserReportListener.java
+++ gui/src/UserReportListener.java
@@ -3,7 +3,6 @@ package src;
 import java.util.List;
 
 public interface UserReportListener {
-	public void okPressed(String comment, List<Item> brokenInventory);
+	public void okPressed(String comment, List<Item> brokenInventory, int usedWood, List<Item> lostItems);
 	public void cancelPressed();
-	
 }
