5af7b607fbe0501e63a279426d6149d0c1752b38
halv00rsen <jorgen-halvorsen@hotmail.com>
2014-11-16 15:21:59 +0100

    en admin må ha et brukernavn på minst 4 bokstaver

--- gui/src/CreateUser.java
+++ gui/src/CreateUser.java
@@ -80,7 +80,7 @@ public class CreateUser extends JPanel{
 	 */
 	private boolean validEmail(){
 		String email = username.getText().toLowerCase().trim();
-		if (adminCheckBox.isSelected() && email != null)
+		if (adminCheckBox.isSelected() && email != null && email.length() > 3)
 			return true;
 		if (email.indexOf("@") == -1 || email.indexOf(".") == -1){
 			JOptionPane.showMessageDialog(null, "Feil format på email");
